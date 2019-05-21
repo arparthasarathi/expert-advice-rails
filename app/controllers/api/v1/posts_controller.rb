2d2cb3b7a641570fe22a2c4d6cd87a31bd5c4eb6
Parthasarathi Raghavan <arparthasarthi@gmail.com>
2019-05-21 19:25:28 +0530

    PostController: included tags and answers in the response for index and show

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,18 +1,18 @@
 module Api
   module V1
     class PostsController < Api::V1::ApiController
-      before_action :doorkeeper_authorize!, except: %i[create index show]
+      before_action :doorkeeper_authorize!, except: %i[create index search show]
 
       def index
         posts = Post.where(question_id: nil).order(:created_at)
 
-        paginate json: posts
+        paginate json: posts, include: ['tags']
       end
 
       def show
         post = Post.find_by(slug: params[:slug])
 
-        render json: post
+        render json: post, include: ['tags', 'answers']
       end
 
       def search
