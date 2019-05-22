64ac2bf7955b5b5c4cba0c9e8b8f9dfdae56d481
Parthasarathi Raghavan <arparthasarthi@gmail.com>
2019-05-22 18:31:21 +0530

    PostModel: added specifications for tags and slug

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,7 @@
 class Post < ApplicationRecord
+  acts_as_taggable_on :tags
+  acts_as_url :title, url_attribute: :slug
+
   belongs_to :user
   belongs_to :account
   belongs_to :question, foreign_key: 'question_id', class_name: 'Post', optional: true
