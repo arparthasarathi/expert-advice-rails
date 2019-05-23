aefe18d948156ed34daca07d3ba5f8982d40b30e
Parthasarathi Raghavan <arparthasarthi@gmail.com>
2019-05-23 18:11:32 +0530

    PostController: added logic for index and show endpoints

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,12 +1,21 @@
 module Api
   module V1
     class PostsController < Api::V1::ApiController
-      before_action :doorkeeper_authorize!, except: %i[create]
+      before_action :doorkeeper_authorize!, except: %i[create index show]
 
       def index
+        posts = Post.where(question_id: nil).order(:created_at)
+
+        paginate json: posts
       end
 
       def show
+        post = Post.find_by(slug: params[:slug])
+
+        render json: post
+      end
+
+      def search
       end
 
       def create
