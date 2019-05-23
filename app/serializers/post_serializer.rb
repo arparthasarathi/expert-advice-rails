d52d1626348d5196a5d0de19b1cf426c7decbc3a
Parthasarathi Raghavan <arparthasarthi@gmail.com>
2019-05-23 19:08:21 +0530

    PostSerializer: added views_count attribute in serializer

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -1,5 +1,5 @@
 class PostSerializer < ActiveModel::Serializer
-  attributes :id, :title, :slug, :body, :question_id
+  attributes :id, :title, :slug, :body, :question_id, :views_count
   has_many :tags
   has_many :answers
   belongs_to :question
