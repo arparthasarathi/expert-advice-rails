c91802b6125191a829a2ec1f0f0f346bcd2bca7a
Parthasarathi Raghavan <arparthasarthi@gmail.com>
2019-05-23 18:45:05 +0530

    PostController: added endpoints for create, update and search

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -16,17 +16,57 @@ module Api
       end
 
       def search
+        term = params[:term]
+        posts = Post.tagged_with(term)
+
+        paginate json: posts
       end
 
       def create
+        post = current_user.posts.build(create_post_params)
+
+        post.set_account(current_account.id)
+
+        if post.save
+          render json: post, account_user: current_account_user
+        else
+          render json: post,
+                 status: :unprocessable_entity,
+                 serializer: ActiveModel::Serializer::ErrorSerializer
+        end
       end
 
       def update
+        post = current_user.posts.find_by(slug: params[:slug])
+
+        if post.present?
+          post.assign_attributes(update_post_params)
+          if post.save
+            render json: post, status: :ok, account_user: current_account_user
+          else
+            render json: post,
+                   status: :unprocessable_entity,
+                   serializer: ActiveModel::Serializer::ErrorSerializer
+          end
+        else
+          render json: post,
+                 status: :forbidden,
+                 serializer: ActiveModel::Serializer::ErrorSerializer
+        end
       end
 
       def destroy
       end
 
+      private
+
+      def create_post_params
+        params.require(:data).require(:attributes).permit(:title, :body, :question_id, :tag_list)
+      end
+
+      def update_post_params
+        params.require(:data).require(:attributes).permit(:id, :title, :body, :question_id, :tag_list)
+      end
     end
   end
 end
