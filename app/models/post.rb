014519558860a2640b37676f0237c219a783c303
Parthasarathi Raghavan <arparthasarthi@gmail.com>
2019-05-23 17:58:52 +0530

    PostModel: added relationships to retrieve question and answers

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :user
   belongs_to :account
+  belongs_to :question, foreign_key: 'question_id', class_name: 'Post', optional: true
+  has_many :answers, foreign_key: 'question_id', class_name: 'Post'
 end
