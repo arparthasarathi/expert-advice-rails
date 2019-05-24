7f962674228ae75e0ba6e3ac3a20f7ede2060fff
Parthasarathi Raghavan <arparthasarthi@gmail.com>
2019-05-25 04:14:50 +0530

    Route: configured search endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
    namespace :api do
     namespace :v1 do
       get '/users/me', to: 'users#me'
+      get '/posts/search', to: 'posts#search'
       resources :users
       resources :posts, param: :slug
     end
