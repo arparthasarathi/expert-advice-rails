a7dcf29ad313be7d804d2bf2f6abdfb2bfe33778
Parthasarathi Raghavan <arparthasarthi@gmail.com>
2019-05-25 04:06:59 +0530

    UserModel: made the changes mentioned below
    - Added posts relationship to the model
    - Added instance methods to build account and account user when
      a new user is created

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,26 @@ class User < ApplicationRecord
 
   has_many :user_account_accesses
   has_many :accounts, through: :user_account_accesses
+  has_many :posts
+
+  before_create :create_account
+
+  ## Function to build and assign account to the user's instance
+  #
+  #  @return [nil]
+  def create_account
+    account = accounts.build
+    create_account_user(account)
+  end
+
+  ## Function to create instance of AccountUser when a new user is
+  #  created
+  #
+  #  @param [Account] account
+  #
+  #  @return [AccountUser]
+
+  def create_account_user(account)
+    AccountUser.new(user: self, account: account)
+  end
 end
